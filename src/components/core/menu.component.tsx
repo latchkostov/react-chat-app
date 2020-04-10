e0d2d996d7c646595f46894993f8771ad29d3575
blgrnboy <latch.kostov@gmail.com>
2020-04-09 20:38:03 -0500

    Add styled components and use wrapper in menu component

--- src/components/core/menu.component.tsx
+++ src/components/core/menu.component.tsx
@@ -7,37 +7,43 @@ import Notifications from '@material-ui/icons/Notifications';
 import Chat from '@material-ui/icons/Chat';
 import Group from '@material-ui/icons/Group';
 import HelpOutline from '@material-ui/icons/HelpOutline';
+import styled from 'styled-components';
 
-
+const Wrapper = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: space-between;
+  flex-direction: column;
+`;
 
 const Menu: FunctionComponent<any> = (props: any) => {
     return (
-        <div>
-            <List>
-              <Link to="/notifications">
-                <ListItem button key='notifications'>
-                  <ListItemIcon><Notifications></Notifications></ListItemIcon>
-                  <ListItemText primary='Notifications' />
-                </ListItem>
-              </Link>
-              <Link to="/direct_messsages">
-                <ListItem button key='chat'>
-                    <ListItemIcon><Chat></Chat></ListItemIcon>
-                    <ListItemText primary='Direct Messages' />
-                </ListItem>
-              </Link>
-              <ListItem button key='channels'>
-                <ListItemIcon><Group></Group></ListItemIcon>
-                <ListItemText primary='Channels' />
-              </ListItem>
-            </List>
-            <List>
-              <ListItem button key='help'>
-                <ListItemIcon><HelpOutline /></ListItemIcon>
-                <ListItemText primary='Help' />
-              </ListItem>
-            </List>
-        </div>
+      <Wrapper>
+        <List>
+          <Link to="/notifications">
+            <ListItem button key='notifications'>
+              <ListItemIcon><Notifications></Notifications></ListItemIcon>
+              <ListItemText primary='Notifications' />
+            </ListItem>
+          </Link>
+          <Link to="/direct_messsages">
+            <ListItem button key='chat'>
+                <ListItemIcon><Chat></Chat></ListItemIcon>
+                <ListItemText primary='Direct Messages' />
+            </ListItem>
+          </Link>
+          <ListItem button key='channels'>
+            <ListItemIcon><Group></Group></ListItemIcon>
+            <ListItemText primary='Channels' />
+          </ListItem>
+        </List>
+        <List>
+          <ListItem button key='help'>
+            <ListItemIcon><HelpOutline /></ListItemIcon>
+            <ListItemText primary='Help' />
+          </ListItem>
+        </List>
+      </Wrapper>
     );
 }
 
